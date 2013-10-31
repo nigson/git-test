5d6c6413e3b0f138332162d763dbd2d224e2fc40
nigson <sddads@dsadas.fi>
2013-10-31 18:13:49 +0200

    Add hello
    Longer text here blaa blaa blaa

---
+++ hello3.php
@@ -0,0 +1,23 @@
+<html> 
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8"></meta>
+<title>Ensimmainen PHP-skripti</title> 
+</head> 
+<body> 
+<!-- hello.php --> 
+<?php
+    echo "Hello, World!";
+		echo "Hello, World!";	
+    echo "Hello, World!";
+		echo "Hello, World!";	    echo "Hello, World!";
+		echo "Hello, World!";	    echo "Hello, World!";
+		echo "Hello, World!";	    echo "Hello, World!";
+		echo "Hello, World!";	    echo "Hello, World!";
+		echo "Hello, World!";	    echo "Hello, World!";
+		echo "Hello, World!";	    echo "Hello, World!";
+		echo "Hello, World!";	    echo "Hello, World!";
+		echo "Hello, World!";	
+		
+		?> 	
+</body> 
+</html>
